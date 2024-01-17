d2ec42980aa2d8f2f0f4633a13c0c801413f8c4f
Erik-Scott567 <156900008+Erik-Scott567@users.noreply.github.com>
2024-01-17 22:19:47 +1100

    Create BookTripController.java

---
+++ controller/Trip/BookTripController.java
@@ -0,0 +1,63 @@
+package controller.Trip;
+
+import au.edu.uts.ap.javafx.*;
+import javafx.stage.*;
+import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.image.Image;
+import model.*;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class BookTripController extends Controller<Agency> {
+
+    public BookTripController() {
+
+    }
+
+    @FXML
+    private Button AddDestBtn;
+    @FXML
+    private Button RemoveBtn;
+    @FXML
+    private Button AddConFlightBtn;
+    @FXML
+    private Button ViewBtn;
+    @FXML
+    private Button CloseBtn;
+
+    @FXML 
+    private void view(ActionEvent event) throws IOException {
+        Stage newStage = new Stage();
+        newStage.getIcons().add(new Image("/image/trip_icon.png"));
+        ViewLoader.showStage(new Stage(),"/view/Trip/DisplayTripView.fxml","View Trip", newStage);
+    }
+
+    @FXML
+    private void Add(ActionEvent event) throws IOException {
+        Stage newStage = new Stage();
+        newStage.getIcons().add(new Image("/image/destinations_icon.png"));
+        ViewLoader.showStage(new Stage(),"/view/Destinations/AddDestinationView.fxml","Add Destination", newStage);
+    }
+
+    @FXML
+    private void Remove(ActionEvent event) throws IOException {
+        Stage newStage = new Stage();
+        newStage.getIcons().add(new Image("/image/destinations_icon.png"));
+        ViewLoader.showStage(new Stage(),"/view/Destinations/RemoveDestinationView.fxml","Remove Destination", newStage);
+    }
+
+    @FXML 
+    private void exit(ActionEvent event) {
+        Stage stage = (Stage) CloseBtn.getScene().getWindow();
+        stage.close();
+    }
+
+}
